52af9f1d76d04b7f49e7de5c19e67f35919826bc
Cristina Lugo <cmlugoce@syr.edu>
2018-06-17 19:15:27 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,13 +4,6 @@ require_relative './fancy_dance.rb'
 
 
 class Kid
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-  
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
